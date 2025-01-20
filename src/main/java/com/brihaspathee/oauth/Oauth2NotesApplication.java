292218f6aa53c9ebf6472bf234eb1eda6a980c8e
vbalaji215-dev <vbalaji215@outlook.com>
2025-01-20 15:00:52 -0500

    Initial Add

---
+++ src/main/java/com/brihaspathee/oauth/Oauth2NotesApplication.java
@@ -0,0 +1,13 @@
+package com.brihaspathee.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2NotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2NotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brihaspathee/oauth/Oauth2NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brihaspathee.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2NotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
